230ef96210313bb00110952f55d1ee010217214e
feilong.cao <feilong.cao@qq.com>
2017-07-03 11:18:36 +0800

    添加个人博客链接

--- app/routes/home/Home.jsx
+++ app/routes/home/Home.jsx
@@ -1,11 +1,13 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import  {Link}  from 'react-router-dom';
 class Home extends Component {
   render() {
     return (
       <div >
-        <div style={{textAlign:'center',marginTop:'50px'}}>
+        <div style={{textAlign: 'center', marginTop: '50px'}}>
           <h2><Link to='/house'>大厂房产查询 </Link></h2>
+
+          <h2><a href='https://caofeilong.github.io/cflblog/'>个人博客 </a></h2>
         </div>
       </div>
     )
